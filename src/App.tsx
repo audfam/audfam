d4d92b157a1bb09d72af46d9b48858233260086f
audfam <audangency@proton.me>
2025-12-28 00:17:20 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -61,11 +61,10 @@ export default function App() {
       
       {/* Header Section */}
       <header className="pt-12 pb-2 w-full flex flex-col items-center h-[80px]">
-        <h1 className="text-[20px] font-bold text-white tracking-tight font-sans">
-          AU Internal Wallet
-        </h1>
-      </header>
-
+       <h1 className="text-[20px] font-semibold tracking-tight text-white font-crypto">
+  AU Internal Wallet
+</h1>
+</header>
       {/* Main Content Card */}
       <main className="flex-1 w-full max-w-[390px] px-6 mt-2">
         <div className="bg-white rounded-[24px] shadow-[0px_10px_40px_rgba(0,0,0,0.5)] min-h-[520px] p-8 flex flex-col items-center animate-in fade-in slide-in-from-bottom-6 duration-700">
@@ -84,10 +83,10 @@ export default function App() {
                 <p className="text-[#1F2937] text-[22px] font-semibold leading-[32px]">
                   Total Balance
                 </p>
-                <p className="text-[#2a7525] text-[8.5px] font-bold tracking-[0.11em] leading-none uppercase">
-                  1,000,000.00 USDT
-                </p>
-              </div>
+<p className="text-[#2a7525] text-[9.5px] font-bold tracking-[0.11em] leading-none uppercase font-mono">
+  1,0
+</p>
+             </div>
 
               {/* Input Section */}
               <div className="w-full mb-8">
