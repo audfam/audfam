7ca2f4ed4f9e748f6873b3bd93e2715ab4a82157
audfam <audangency@proton.me>
2025-12-28 00:31:21 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -61,7 +61,7 @@ export default function App() {
       
       {/* Header Section */}
       <header className="pt-12 pb-2 w-full flex flex-col items-center h-[80px]">
-       <h1 className="text-[16px] font-semibold tracking-tight text-white font-crypto">
+       <h1 className="text-[16px] font-semibold tracking-tight text-[#2a7525] font-crypto">
   AU Internal Wallet Finance Department Only
 </h1>
 </header>
